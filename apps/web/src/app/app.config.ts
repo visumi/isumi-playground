5731289041d994d6b9e51642219401162a248b57
Vinicius Isumi <60152427+visumi@users.noreply.github.com>
2026-09-10 16:57:38 -0300

    feat(web): habilita suporte PWA

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { DATE_PIPE_DEFAULT_OPTIONS, registerLocaleData } from "@angular/common";
 import localePt from "@angular/common/locales/pt";
-import { ApplicationConfig, LOCALE_ID } from "@angular/core";
+import { ApplicationConfig, isDevMode, LOCALE_ID } from "@angular/core";
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from "@angular/router";
+import { provideServiceWorker } from "@angular/service-worker";
 import { routes } from "./app.routes";
 import { authInterceptor } from "./core/auth/auth.interceptor";
 
@@ -16,6 +17,10 @@ export const appConfig: ApplicationConfig = {
       withInMemoryScrolling({ scrollPositionRestoration: "top" })
     ),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideServiceWorker("ngsw-worker.js", {
+      enabled: !isDevMode(),
+      registrationStrategy: "registerWhenStable:30000"
+    }),
     {
       provide: DATE_PIPE_DEFAULT_OPTIONS,
       useValue: { timezone: "-0300" }
